5b583468b31a9323590e4b33f0dd852890578c2e
Abdulqadir53Mohamedali <abdulqadir.tm@gmail.com>
2023-09-22 20:00:39 +0100

    Comments added

--- Python-Mini-Projects/Hangman.py
+++ Python-Mini-Projects/Hangman.py
@@ -1,36 +1,54 @@
-#secret_word = input('Please type in a word for your friend to choose:').lower()
+# Initializing the secret word that the user has to guess
 secret_word = 'word'
+# To keep track of letters guessed by the user
 lettercount = ""
+# Number of incorrect attempts left
 failed_count = 6
 
 def hangman():
+    # Informing the user about the format of the secret word
     print('No capital letter are included within the secret word')
+    # Printing a separator for visual clarity
     print("""""""""""""")
+    # Resetting the failed count for each game
     failed_count = 6
+    
+    # While loop continues as long as there are attempts left
     while failed_count > 0:
+        # Asking user for their letter guess
         guess = input('Enter a letter:')
+        
+        # Checking if guessed letter is in the secret word
         if guess in secret_word:
             print(f'Correct!, there is one or more {guess} in the secret word')
         else:
+            # If the guess is incorrect, decrementing the failed attempts count
             failed_count -= 1
             print(f'Incorrect! The letter {guess} is not in the secret word')
             
+        # Adding guessed letter to lettercount to track user's guesses
         lettercount += guess
         wrong_letter_count = 0
         
+        # Displaying the current status of the word being guessed
         for letter in secret_word:
             if letter in lettercount:
                 print(f"{letter}", end="")
             else:
+                # If the letter hasn't been guessed yet, printing an underscore
                 print("_", end="",)
                 wrong_letter_count += 1
                 
+        # Checking if the user has guessed the word correctly
         if wrong_letter_count == 0:
             print(f"\nCongratulations!, the secret word was: {secret_word}, you won ")
             break
+    # This block executes if the user runs out of attempts
     else:
         print("\nYou did not win this time, unlucky")
         print("""""""""""""""""""""""""""""""""""""""""""""""""""""""""""")
+        # Starting a new game
         hangman()
 
+# Starting the game
 hangman()
--- Python-Mini-Projects/Slot-Machine.py
+++ Python-Mini-Projects/Slot-Machine.py
@@ -1,60 +1,64 @@
-MAX_LINES=3
-MIN_BET=1
-MAX_BET=100
+# Constants for the game's settings
+MAX_LINES = 3
+MIN_BET = 1
+MAX_BET = 100
 
-def deposit ():
-   while True:
-     amount=input('what would you like to deposit? £')
-     if amount.isdigit():
-       amount1=int(amount)
-     if amount1 > 0:
-         break
-     else:
-          print('amount must be greater than zero')
-   else:
-       print('please enter a number')
-   return amount1
-
-
+def deposit():
+    """Prompt the user to deposit an amount and validate the input."""
+    while True:
+        amount = input('What would you like to deposit? £')
+        if amount.isdigit():
+            amount1 = int(amount)
+            if amount1 > 0:
+                break
+            else:
+                print('Amount must be greater than zero')
+        else:
+            print('Please enter a number')
+    return amount1
 
 def lines():
-  while True:
-     balance=input(f'How many lines would you like to bet on (1-{MAX_LINES})? ')
-     if balance.isdigt():
-       lines= int(balance)
-       if 1 <= lines <= MAX_LINES :
-         break
-       else:
-         print('Enter a valid number of lines ')
-     else:
-       print('please enter a number')
-  return lines
+    """Prompt the user to choose the number of lines to bet on and validate the input."""
+    while True:
+        balance = input(f'How many lines would you like to bet on (1-{MAX_LINES})? ')
+        if balance.isdigit():
+            lines = int(balance)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print('Enter a valid number of lines ')
+        else:
+            print('Please enter a number')
+    return lines
 
-
-def get_bet ():
-  while True:
-     bet=input('how much would you like to bet? £')
-     if bet.isdigt():
-       amount= int(bet)
-       if MIN_BET <=amount<= MAX_BET > 0:
-         break
-       else:
-         print(f'Amount bet must be between{MIN_BET}-{MAX_BET}')
-     else:
-       print('please enter a number')
-  return amount
-
+def get_bet():
+    """Prompt the user for their bet amount and validate the input."""
+    while True:
+        bet = input('How much would you like to bet? £')
+        if bet.isdigit():
+            amount = int(bet)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f'Amount bet must be between {MIN_BET}-{MAX_BET}')
+        else:
+            print('Please enter a number')
+    return amount
 
 def main():
-  balance=deposit()
-  lin1= lines()
-  while True :
-    bet=get_bet()
-    Total_bet= bet*lin1
-    if Total_bet > balance:
-      print(f'You do not have enough to bet that amount,your balance is {balance}')
-    else:
-      break
-     
+    """Main game loop."""
+    # Get the initial deposit from the user
+    balance = deposit()
+    # Get the number of lines the user wants to bet on
+    lin1 = lines()
+    while True:
+        # Get the bet amount from the user
+        bet = get_bet()
+        Total_bet = bet * lin1
+        if Total_bet > balance:
+            print(f'You do not have enough to bet that amount, your balance is {balance}')
+        else:
+            break
 
+# Start the game
 main()
